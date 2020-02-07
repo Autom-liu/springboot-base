c24fb2eca658252bfd6785571cfe15e696dc14a1
Autom-liu <349576362@163.com>
2020-02-07 23:44:46 +0800

    接口异常的抛出

--- springboot-base-baseinfo/src/main/java/com/edu/scnu/baseinfo/web/api/AccountApi.java
+++ springboot-base-baseinfo/src/main/java/com/edu/scnu/baseinfo/web/api/AccountApi.java
@@ -15,11 +15,11 @@ import com.edu.scnu.baseinfo.dto.AccountDTO;
 import com.edu.scnu.baseinfo.query.AccountQuery;
 import com.edu.scnu.baseinfo.service.AccountService;
 import com.edu.scnu.baseinfo.vo.AccountVO;
+import com.edu.scnu.common.exception.BizException;
 import com.edu.scnu.common.vo.IResult;
 import com.edu.scnu.common.vo.Result;
 import com.edu.scnu.common.vo.ResultList;
 import com.edu.scnu.common.web.annotation.CommonRequestBody;
-import com.edu.scnu.common.web.annotation.PermissionRequire;
 
 @RestController
 public class AccountApi {
@@ -27,22 +27,20 @@ public class AccountApi {
 	@Autowired
 	private AccountService accountService;
 
-	@PermissionRequire
 	@PostMapping("/account/add")
-	public IResult addAccount(@Valid @CommonRequestBody AccountDTO dto) {
-		// TODO 必传字段校验
+	public IResult addAccount(@Valid @CommonRequestBody AccountDTO dto) throws BizException {
 		accountService.insert(dto);
 		return Result.success("");
 	}
 	
 	@DeleteMapping("/account/{id}")
-	public IResult deleteAccount(@PathVariable("id") Integer id) {
+	public IResult deleteAccount(@PathVariable("id") Integer id) throws BizException {
 		accountService.deleteById(id);
 		return Result.success("");
 	}
 	
 	@GetMapping("/account/{id}")
-	public IResult findOne(@PathVariable("id") Integer id) {
+	public IResult findOne(@PathVariable("id") Integer id) throws BizException {
 		AccountVO accountVO = accountService.findOne(id);
 		return Result.success(accountVO);
 	}
