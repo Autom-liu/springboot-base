2973c71bf9e267f3574b0c216ce5c14d3216387e
Autom-liu <349576362@163.com>
2021-02-15 09:01:40 +0800

    搭建admin模块

---
+++ springboot-base-admin/src/main/java/com/edu/scnu/WebAdminApplication.java
@@ -0,0 +1,9 @@
+package com.edu.scnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class WebAdminApplication {
+
+
+}
